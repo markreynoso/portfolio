953bfb3f7cdb34c636667712aa67027b4142be77
Mark Reynoso <mreynoso@spu.edu>
2017-09-15 10:10:14 -0700

    moved data to sourcedata.js

--- app.js
+++ app.js
@@ -57,10 +57,6 @@ function Edu (school, degree, year) {
   this.year = year;
 }
 
-new Project ('Procrasinator Generator', 'https://willjreid.github.io/procrastinate/quiz.html', 'images/procrastinate.png', '2017', 'Code Fellows', 'website', 'For those times when you need a break from your busy schedule you can use the Procrasinator Generator. Don\'t waste more time than you have. This was a team project at Code Fellows at the completion of a 201 level course.');
-new Project ('Bus Mall', 'https://markreynoso.github.io/bus-mall/', 'images/busmall.png', '2017', 'Code Fellows', 'website', 'Bus Mall takes a look at a mythical magazine that is looking for customer feedback regarding which products readers may have an interest in purchasing. This was a solo project during week 3 at Code Fellows 201 level course.');
-new Project ('Salmon Cookies', 'https://markreynoso.github.io/cookie-stand/', 'images/salmoncookies.png', '2017', 'Code Fellows', 'website', 'Salmon Cookies produces reports for the imaginary cookie store selling cookies made of Salmon. This was a second week project in a 201 level course at Code Fellows.');
-
 projects.forEach(function(projects) {
   $('#projects').append(projects.toHtml());
 });
--- sourcedata.js
+++ sourcedata.js
@@ -1,2 +1,32 @@
 'use strict';
 var projects = [];
+
+var projectData = [{
+  title: 'Procrasinator Generator',
+  link: 'https://willjreid.github.io/procrastinate/quiz.html',
+  img: 'images/procrastinate.png',
+  year: '2017',
+  client: 'Code Fellows',
+  type: 'website',
+  description: 'For those times when you need a break from your busy schedule you can use the Procrasinator Generator. Don\'t waste more time than you have. This was a team project at Code Fellows at the completion of a 201 level course.'
+}, {
+  title: 'Bus Mall',
+  link: 'https://markreynoso.github.io/bus-mall/',
+  img: 'images/busmall.png',
+  year: '2017',
+  client: 'Code Fellows',
+  type: 'website',
+  description: 'Bus Mall takes a look at a mythical magazine that is looking for customer feedback regarding which products readers may have an interest in purchasing. This was a solo project during week 3 at Code Fellows 201 level course.'
+}, {
+  title: 'Salmon Cookies',
+  link: 'https://markreynoso.github.io/cookie-stand/',
+  img: 'images/salmoncookies.png',
+  year: '2017',
+  client: 'Code Fellows',
+  type: 'website',
+  description: 'Salmon Cookies produces reports for the imaginary cookie store selling cookies made of Salmon. This was a second week project in a 201 level course at Code Fellows.'
+}];
+
+// new Project ('Salmon Cookies', 'https://markreynoso.github.io/cookie-stand/', 'images/salmoncookies.png', '2017', 'Code Fellows', 'website', 'Salmon Cookies produces reports for the imaginary cookie store selling cookies made of Salmon. This was a second week project in a 201 level course at Code Fellows.');
+// new Project ('Bus Mall', 'https://markreynoso.github.io/bus-mall/', 'images/busmall.png', '2017', 'Code Fellows', 'website', 'Bus Mall takes a look at a mythical magazine that is looking for customer feedback regarding which products readers may have an interest in purchasing. This was a solo project during week 3 at Code Fellows 201 level course.');
+// new Project ('Procrasinator Generator', 'https://willjreid.github.io/procrastinate/quiz.html', 'images/procrastinate.png', '2017', 'Code Fellows', 'website', 'For those times when you need a break from your busy schedule you can use the Procrasinator Generator. Don\'t waste more time than you have. This was a team project at Code Fellows at the completion of a 201 level course.');
