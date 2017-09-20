bb74afae9d21b7448f91f657f46f8c04ac27250a
Mark Reynoso <mreynoso@spu.edu>
2017-09-20 09:42:36 -0700

    initialized package, added express and body-parser, moved front end to public directory, created response to get request for home page and any other page as 404

---
+++ server.js
@@ -0,0 +1,20 @@
+'use strict';
+
+const EXPRESS = require('EXPRESS');
+const PARSER = require('body-parser').urlencoded({extended: true});
+const APP = EXPRESS();
+const PORT = process.env.PORT || 3000;
+
+APP.use(EXPRESS.static('public'));
+
+APP.get('/', function(request, response){
+  response.sendFile('index.html', {root: './public'});
+})
+
+APP.get('*', function(request, response){
+  response.sendFile('404.html', {root: './public'});
+})
+
+APP.listen(PORT, function(){
+  console.log(`Express server currently running on port ${PORT}`);
+})
