bbfa8da7fd3fd628787356dbd11d76244e899e64
Mark Reynoso <mreynoso@spu.edu>
2018-01-16 10:58:29 -0800

    cleaned up corpse code

--- public/scripts/views/resume.js
+++ public/scripts/views/resume.js
@@ -1,20 +1,5 @@
 'use strict';
-// var work = [];
 var design = [];
-
-// function Job (info) {
-//   this.title = info.title;
-//   this.start = info.start;
-//   this.end = info.end;
-//   this.summary = info.summary;
-//   this.company = info.company;
-//   this.used = info.used;
-// }
-// Job.prototype.toHtml = function() {
-//   var theHtml = $('#job-template').html();
-//   var createTemplate = Handlebars.compile(theHtml);
-//   return createTemplate(this);
-// }
 
 function Design (data) {
   this.title = data.title;
@@ -36,25 +21,7 @@ Design.prototype.toHtml = function(){
 designData.forEach(function(projects){
   design.push(new Design(projects));
 })
+
 design.forEach(function(project){
   $('#experience').append(project.toHtml());
 })
-
-
-// // jobs
-// jobData.forEach(function(jobList) {
-//   work.push(new Job(jobList));
-// });
-// work.forEach(function(job) {
-//   $('#experience').append(job.toHtml());
-// });
-
-// let newData = jobData.map(thing => thing.used).reduce(function(a, b){
-//   return a + b;
-// });
-// $('#experience').append(`<h4>Skills</h4><p>${newData}</p>`);
-
-//github repos
-// function getsMyGit(){
-//   $.get('/github/users/markreynoso/repos').then(response => response.forEach(git => $('.git').append(`<h4>${git.name}</h4><p>${git.created_at}</p><p><a href="${git.url}">see repo</a></p></br>`)))
-// }
